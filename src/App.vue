40a30f56a95cf8b812fe523a26c3d3673bd35585
nicktu12 <nickteets@gmail.com>
2019-02-14 15:41:37 -0700

    Green

--- src/App.vue
+++ src/App.vue
@@ -9,7 +9,7 @@
           <p class="text-sm leading-tight text-grey-dark">Developer at NothingWorks Inc.</p>
         </div>
         <div>
-          <button class="text-xs font-semibold rounded-full px-4 py-1 leading-normal bg-white border border-purple text-purple hover:bg-purple hover:text-white">Message</button>
+          <button class="text-xs font-semibold rounded-full px-4 py-1 leading-normal bg-white border border-green text-green hover:bg-green hover:text-white">Message</button>
         </div>
       </div>
     </div>
